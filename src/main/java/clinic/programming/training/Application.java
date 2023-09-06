89df58c237739a16c19b130d2a6c260889aa7913
Bil Hamund <liamhamundr@me.com>
2023-09-05 19:45:28 -0700

    changed compiler plugin to Java source 17

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,7 +1,34 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     
+    // The sealed class Animal
+public sealed class Animal permits Dog {
+    public void makeSound() {
+        System.out.println("Some generic animal sound");
+    }
+}
+
+// A permitted subclass of Animal
+public final class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Woof! Woof!");
+    }
+}
+
+    public void greet() {
+        List<String> greetings = new ArrayList<>();
+        greetings.add("Hello, ");
+
+        for (String greeting : greetings) {
+            System.out.println("Greetings" + greeting);
+        }
+    }
+
     public Application() {
         System.out.println ("Inside Application");
     }
@@ -10,5 +37,6 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	    Application app = new Application();
+      app.greet();
     }
 }
